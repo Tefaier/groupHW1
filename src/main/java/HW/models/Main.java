52df774b3cb3a99f4208632a6febcf5f3a0c641b
PaulVBelo <144692494+PaulVBelo@users.noreply.github.com>
2023-12-21 17:39:54 +0300

    Add files via upload

---
+++ src/main/java/HW/models/Main.java
@@ -0,0 +1,32 @@
+package HW.models;
+
+import HW.models.basket.BasketRepository;
+import HW.models.basket.BasketRepositoryInMemory;
+import HW.models.enums.Product;
+import HW.models.parser.CommandParserTXT;
+import HW.models.storage.Storage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class Main {
+  private static final Logger logger = Logger.getLogger(Main.class.getSimpleName());
+
+  public static void main(String[] args) {
+    Map<Product, Float> storageContent = new HashMap<Product, Float>();
+    storageContent.put(Product.Bread, 100f);
+    storageContent.put(Product.Flour, 100f);
+    storageContent.put(Product.Water, 100f);
+    Storage storage = new Storage(storageContent);
+    BasketRepository basketRepository = new BasketRepositoryInMemory(storage);
+    String path = "commands/list1.txt";
+    try {
+      CommandParserTXT parser = new CommandParserTXT(path, basketRepository);
+      parser.applyAllCommands();
+    } catch (IOException e) {
+      logger.warning("Failed while executing commands from txt file: " + e.getMessage());
+    }
+  }
+}
