a4ba0d37b0cfab959a1b2c8aacc639014242bf81
PaulVBelo <144692494+PaulVBelo@users.noreply.github.com>
2023-12-21 17:41:06 +0300

    Add files via upload

---
+++ src/main/java/HW/models/basket/BasketRepositoryInMemory.java
@@ -0,0 +1,55 @@
+package HW.models.basket;
+
+import HW.models.enums.Product;
+import HW.models.storage.Storage;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
+public class BasketRepositoryInMemory implements BasketRepository{
+  private static final Logger logger = Logger.getLogger(BasketRepositoryInMemory.class.getSimpleName());
+  private final ConcurrentHashMap<Long, Basket> baskets = new ConcurrentHashMap<>();
+  private final Storage storage;
+
+  public BasketRepositoryInMemory(Storage storage) {
+    this.storage = storage;
+  }
+
+  @Override
+  public void changeBasketContent(long id, Product product, float change) {
+    synchronized (Long.valueOf(id)) {
+      logger.info("Basket with id " + id + " was changed: product->" + product.displayName + " change->" + change + " " + product.unitName);
+      if (!baskets.containsKey(id)) {
+        baskets.put(id, new Basket());
+      }
+      try {
+        baskets.get(id).alterContent(product, change);
+      } catch (IllegalArgumentException e) {
+        logger.info("Change to basket with id " + id + " wasn't made");
+      }
+    }
+  }
+
+  @Override
+  public void tryOrderBasket(long id) {
+    synchronized (Long.valueOf(id)) {
+      logger.info("Basket with id " + id + " was ordered");
+      try {
+        Map<Product, Float> basketContent = baskets.get(id).getProducts();
+        this.storage.takeAway(basketContent);
+        baskets.get(id).clearBasket();
+      } catch (RuntimeException e) {
+        logger.info("Basket with id " + id + " was ordered but failed!");
+      }
+    }
+  }
+
+  @Override
+  public void removeBasket(long id) {
+    synchronized (Long.valueOf(id)) {
+      logger.info("Basket with id " + id + " was removed");
+      baskets.remove(id);
+    }
+  }
+}
